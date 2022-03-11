d5ceee8b83a3517bb8eefa85d1b4a2a0d2cb4e01
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2022-03-11 17:58:26 +0900

    chapter 2

--- src/chapter_2.rs
+++ src/chapter_2.rs
@@ -6,7 +6,7 @@ struct User {
     owned_items: Vec<Item>,
 }
 
-const DEFULT_WALLET_AMMOUNT: f64 = 500000.0;
+const DEFULT_WALLET_AMMOUNT: f64 = 250000.0;
 
 impl User {
     fn new(name: String) -> User {
@@ -26,13 +26,14 @@ impl fmt::Display for User {
             self.wallet,
             self.owned_items
                 .iter()
-                .map(|item| format!("{item}"))
+                .map(|item| item.to_string())
                 .collect::<Vec<_>>()
                 .join(", ")
         ))
     }
 }
 
+#[derive(Clone)]
 struct Item {
     name: String,
     price: f64,
@@ -44,6 +45,10 @@ impl Item {
             name: name.to_string(),
             price,
         }
+    }
+
+    fn total_price(items: &[Self]) -> f64 {
+        items.iter().map(|item| item.price).sum()
     }
 
     fn default_stocks() -> Vec<Item> {
@@ -61,16 +66,61 @@ impl fmt::Display for Item {
     }
 }
 
+impl PartialEq for Item {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
 // なるべくpureなrustで楽なエラーハンドリングをするためのエイリアス
 // 本来はanyhowなどのライブラリを導入した方が現実の開発の上では便利になる
 type Error = Box<dyn std::error::Error>;
 
+#[derive(Debug)]
+enum GeekpError {
+    InsufficientMoney,
+}
+
+impl fmt::Display for GeekpError {
+    fn fmt(&self, w: &mut fmt::Formatter) -> fmt::Result {
+        w.write_fmt(format_args!("{:?}", self))
+    }
+}
+impl std::error::Error for GeekpError {}
+
+fn buy(user: &mut User, items: Vec<Item>, stocks: &mut Vec<Item>) -> Result<(), Error> {
+    let total_price = Item::total_price(&items);
+    if total_price > user.wallet {
+        return Err(GeekpError::InsufficientMoney.into());
+    }
+
+    for item in items {
+        stocks.retain(|stock| stock == &item);
+        user.owned_items.push(item);
+    }
+    user.wallet -= total_price;
+
+    Ok(())
+}
+
 fn main() -> Result<(), Error> {
     let name = inquire::Text::new("あなたのお名前は?").prompt()?;
-    let user = User::new(name);
-    let stocks = Item::default_stocks();
+    let mut user = User::new(name);
+    let mut stocks = Item::default_stocks();
     println!("{}", user);
-    let _cart = inquire::MultiSelect::new("買いたい商品を選んでください", stocks).prompt();
+    let cart =
+        inquire::MultiSelect::new("買いたい商品を選んでください", stocks.clone()).prompt()?;
+
+    buy(&mut user, cart, &mut stocks)?;
+
     println!("{}", user);
+    println!(
+        "{}",
+        stocks
+            .iter()
+            .map(|stock| format!("{stock}"))
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
     Ok(())
 }
